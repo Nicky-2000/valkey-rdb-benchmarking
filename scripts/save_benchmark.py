f6a25a2dc14a5f8dea0e7750c46dd3141dba9cbb
Nicky Khorasani <nickykhorasani@google.com>
2025-08-13 23:28:20 +0000

    improve benchmakr

--- scripts/save_benchmark.py
+++ scripts/save_benchmark.py
@@ -21,7 +21,11 @@ from utilities.valkey_server_utilities import (
     stop_valkey_server,
     wait_for_server_to_start,
 )
-from utilities.populate_server import populate_data_standalone, KEY_SIZE_BYTES, populate_data_with_benchmark
+from utilities.populate_server import (
+    populate_data_standalone,
+    KEY_SIZE_BYTES,
+    populate_data_with_benchmark,
+)
 from utilities.valkey_commands import get_db_key_count, profile_blocking_save
 from utilities.flamegraph_profiler import FlamegraphProfiler
 
@@ -40,11 +44,11 @@ def save_benchmark(config: BenchmarkConfig, output_dir: Path):
         compression_options = []
         if config.rdb_compression == "both":
             compression_options = ["yes", "no"]
-        else: 
+        else:
             compression_options.append(config.rdb_compression)
-            
-        config.rdb_compression = "yes" # Set this so we can start the server
-        
+
+        config.rdb_compression = "yes"  # Set this so we can start the server
+
         process = start_standalone_valkey_server(config)
         if not process:
             return None
@@ -60,7 +64,6 @@ def save_benchmark(config: BenchmarkConfig, output_dir: Path):
         num_keys_expected = config.num_keys_millions * 1e6
         # keys_to_test = populate_data_standalone(config)
         initial_key_count = get_db_key_count(config)
-        print(f"INITIAL KEY COUNT: {initial_key_count}")
 
         if initial_key_count != num_keys_expected:
             logging.error(
@@ -70,17 +73,25 @@ def save_benchmark(config: BenchmarkConfig, output_dir: Path):
 
         # --- 3. Iterate, Profile, and Collect Results ---
         all_results = []
-        thread_counts_to_test = [1, 2, 3, 4, 6, 8, 10]
-        
+        thread_counts_to_test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14]
+
         for comp_option in compression_options:
-            logging.info(colorize(f"--- Running test with rdbcompression = {comp_option} ---", LOG_COLORS.CYAN))
+            logging.info(
+                colorize(
+                    f"--- Running test with rdbcompression = {comp_option} ---",
+                    LOG_COLORS.CYAN,
+                )
+            )
             # Enables us to only have to load keys one time to test save with both compression on and off
-            config.rdb_compression = comp_option # For data saving
-            client.config_set("rdbcompression", comp_option) 
-            
+            config.rdb_compression = comp_option  # For data saving
+            client.config_set("rdbcompression", comp_option)
+
             for num_threads in thread_counts_to_test:
-                logging.info(colorize(
-                    f"--- Running test with rdb-threads = {num_threads} ---", LOG_COLORS.CYAN)
+                logging.info(
+                    colorize(
+                        f"--- Running test with rdb-threads = {num_threads} ---",
+                        LOG_COLORS.CYAN,
+                    )
                 )
                 client.config_set("rdb-threads", num_threads)
 
@@ -88,9 +99,11 @@ def save_benchmark(config: BenchmarkConfig, output_dir: Path):
                     # Use the FlamegraphProfiler context manager
                     # Conditionally run the profiler if the flag is set
                     if config.gen_flamegraph:
-                        with FlamegraphProfiler(pid=process.pid, output_dir=output_dir) as profiler:
+                        with FlamegraphProfiler(
+                            pid=process.pid, output_dir=output_dir
+                        ) as profiler:
                             save_result = profile_blocking_save(client, process)
-                        
+
                         flamegraph_file_name = f"flamegraph_{num_threads}threads.svg"
                         profiler.generate(flamegraph_file_name)
                     else:
@@ -106,18 +119,21 @@ def save_benchmark(config: BenchmarkConfig, output_dir: Path):
                 # --- 4. Process and Aggregate Results ---
                 data_dir = Path(config.temp_dir) / f"node_data_{config.start_port}"
                 rdb_file_path = data_dir / "dump.rdb"
-                
+
                 rdb_file_size_bytes = (
                     rdb_file_path.stat().st_size if rdb_file_path.exists() else 0
                 )
-                
+
                 save_duration = save_result.get("save_duration_seconds", 0)
                 actual_throughput = 0
                 valkey_data_throughput = 0
                 num_keys = int(config.num_keys_millions * 1e6)
                 if save_result.get("status") == "ok" and save_duration > 0:
                     actual_throughput = (rdb_file_size_bytes / save_duration) * (10**-6)
-                    valkey_data_throughput =((num_keys * (config.value_size_bytes + KEY_SIZE_BYTES)) / save_duration) * (10**-6)
+                    # valkey_data_throughput = (
+                    #     (num_keys * (config.value_size_bytes + KEY_SIZE_BYTES))
+                    #     / save_duration
+                    # ) * (10**-6)
 
                 # Combine all results into a single dictionary
                 final_result = {
@@ -137,7 +153,7 @@ def save_benchmark(config: BenchmarkConfig, output_dir: Path):
                     f"Finished test for {num_threads} threads. Deleting RDB file for next run."
                 )
                 delete_file(str(rdb_file_path))
-        
+
         logging.info("--- All benchmark iterations completed ---")
         return all_results
 
@@ -163,12 +179,18 @@ def main():
         run_id = time.strftime("%Y%m%d_%H%M%S")
         project_root = Path(__file__).resolve().parents[1]
         # Make directory name more descriptive
-        dir_name = "save_benchmark_with_flamegraphs" if config.gen_flamegraph else "save_benchmark"
+        dir_name = (
+            "save_benchmark_with_flamegraphs"
+            if config.gen_flamegraph
+            else "save_benchmark"
+        )
         output_dir = project_root / "results" / f"{dir_name}_{run_id}"
         output_dir.mkdir(parents=True, exist_ok=True)
         logging.info(f"All output for this run will be saved in: {output_dir}")
     except Exception as e:
-        logging.critical(f"Failed to create output directory. Aborting. Error: {e}", exc_info=True)
+        logging.critical(
+            f"Failed to create output directory. Aborting. Error: {e}", exc_info=True
+        )
         return
 
     logging.info("--- Starting RDB SAVE Benchmark ---")
@@ -181,7 +203,7 @@ def main():
             f"save_summary_{config.num_keys_millions}keys_{config.value_size_bytes}B"
             f"_comp-{config.rdb_compression}_csum-{config.rdb_checksum}.csv"
         )
-        
+
         save_results_to_csv(
             results=results,
             output_dir=str(output_dir),

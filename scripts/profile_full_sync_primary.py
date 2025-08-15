782a91da46c4525870a27b9c5cc8405ca3f950dd
Nicky Khorasani <nickykhorasani@google.com>
2025-08-15 18:11:48 +0000

    fix log message

--- scripts/profile_full_sync_primary.py
+++ scripts/profile_full_sync_primary.py
@@ -72,7 +72,7 @@ def profile_primary_bgsave(
                 if not line:
                     time.sleep(0.1)
                     continue
-                match = re.search(r'started by pid (\d+) to pipe', line)
+                match = re.search(r'Background RDB transfer started by pid', line)
                 if match:
                     child_pid = int(match.group(1))
                     logging.info(colorize(f"Detected BGSAVE child process with PID: {child_pid}.", LOG_COLORS.GREEN))
